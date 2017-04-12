d27cbec7493c32dae36f93e9c742ea05a12513f9
zeldemire <zeldemire@gmail.com>
2017-04-12 19:22:07 -0400

    Finished everything. Ask teacher about constructors

--- myset.cpp
+++ myset.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 #include "myset.h"
 
 int main(int argc, char *argv[]) {
-    int Array[] = {3,6,29,5,11};
-    int *overwrite_start = std::remove_if(std::begin(Array), std::end(Array), [](int n){ return n==29; });
-    std::fill(overwrite_start, std::end(Array), -1);
-//    std::cout << *overwrite_start;
-    for (int i = 0; i < 5; ++i) {
-        std::cout << Array[i];
-    }
+    myset test1;
+    myset test2;
+    test2 = test1;
+    test1.insert(2);
+
+    std::cout << test1.data[0];
+//    myset1.insert(21);
+//    std::cout << myset1.isElement(2) << std::endl;
+//    std::cout << myset1.capacity() << std::endl;
+//    std::cout << myset1.size() << std::endl;
+//    myset1.remove(21);
+//    std::cout << myset1.size() << std::endl;
+//
+//    for (int i = 0; i < myset1.size(); ++i) {
+//        std::cout << myset1.data[i] << std::endl;
+//    }
+    int a = 0;
 }
--- myset.h
+++ myset.h
@@ -12,60 +12,62 @@ const bool DUMP = true;
 class myset {
 public:
     static const int MIN_CAPACITY = 10;
-    int sizeOfArray = 0;
 
     int *data;	// must be dynamically allocate 1D array
+
     myset() {
         data = new int [MIN_CAPACITY];
     }
     ~myset() {
-        delete[](data);
+        delete[] data;
     }
 
     // copy constructor
     myset(const myset &other) {
-        data = other.data;
+        std::copy(other.data, other.data + other.size(), data);
     }
 
     // Assignment operator
-    myset &operator = (const myset &other) {
-        myset tmp(other);
-        std::swap(data, tmp.data);
+    myset &operator = (myset &other) {
+        std::swap(this->data, other.data);
         return *this;
     }
 
     // move constructor
     myset(myset &&other) {
         data = other.data;
+
         other.data = nullptr;
     }
 
     // move assignment constructor
     myset &operator = (myset &&other) 	{
-        delete[](data);
-
-        data = other.data;
-
-        other.data = nullptr;
+        if (this != &other) {
+            delete[] data;
+            data = other.data;
+            other.data = nullptr;
+        }
 
         return *this;
     }
 
     // returns the number of elements in the set.
     int size() const {
-        return sizeOfArray;
+        return this->sizeOfArray;
     }
 
     // Returns the current size of the array.
     int capacity() const {
-        return sizeof(data);
+        return sizeof(int) * size();
     }
 
     // Removes a value to the set. Return true iff the element
     // was successfully removed.
     bool remove(int value) {
-        if (isElement(value)) {
-
+        if (isElement(value) == 1) {
+            std::remove_copy(this->data, this->data + this->size(), this->data, value);
+            this->sizeOfArray--;
+            return true;
         }
         return false;
     }
@@ -73,18 +75,19 @@ public:
     // Adds a value to the set. Return true iff the element
     // was successfully added.
     bool insert(int value) {
-//        sizeOfArray++;
+        if (size() < MIN_CAPACITY) {
+            for (int i = this->size(); i <= this->size(); ++i) {
+                this->data[i] = value;
+            }
+            this->sizeOfArray++;
+            return true;
+        }
         return false;
     }
 
     // Returns true if value is part of set.
     bool isElement(int value) const {
-        for (int i = 0; i < size(); ++i) {
-            if (value == data[i]) {
-                return true;
-            }
-        }
-        return false;
+        return std::find(this->data, this->data + this->size(), value) != this->data + this->size();
     }
 
     // Returns an array containing the elements in the set.
@@ -100,6 +103,11 @@ public:
         result[4] = 40;
         return result;
     }
+
+
+
+private:
+    int sizeOfArray = 0;
 };
 
 #endif
